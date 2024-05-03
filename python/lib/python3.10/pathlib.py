380b6c3c1db322e2cad48ba209fa4a6be2a320af
ColdWindScholar <3590361911@qq.com>
2024-05-03 13:02:02 +0800

    init

--- python/lib/python3.10/pathlib.py
+++ python/lib/python3.10/pathlib.py
@@ -1,3 +1,6 @@
+import sys
+from errno import ENOENT, ENOTDIR, EBADF, ELOOP
+
 import fnmatch
 import functools
 import io
@@ -5,14 +8,11 @@ import ntpath
 import os
 import posixpath
 import re
-import sys
 import warnings
 from _collections_abc import Sequence
-from errno import EINVAL, ENOENT, ENOTDIR, EBADF, ELOOP
 from operator import attrgetter
 from stat import S_ISDIR, S_ISLNK, S_ISREG, S_ISSOCK, S_ISBLK, S_ISCHR, S_ISFIFO
 from urllib.parse import quote_from_bytes as urlquote_from_bytes
-
 
 __all__ = [
     "PurePath", "PurePosixPath", "PureWindowsPath",
